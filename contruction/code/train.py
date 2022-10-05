150f333dbd88f6af348c4b483bb6be2b65c352c6
damti <damtien444@gmail.com>
2022-10-05 23:37:59 +0700

    feat: implement first version unet and basic train pipeline

---
+++ contruction/code/train.py
@@ -0,0 +1,119 @@
+import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from tqdm import tqdm
+import torch.nn as nn
+import torch.optim as optim
+from model import UNET
+from datetime import datetime
+
+from utils import load_checkpoint, save_checkpoint, get_loaders, check_accuracy, save_predictions_as_imgs
+
+# Hyperparameter etc.
+
+LEARNING_RATE = 1e-4
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 4
+NUM_EPOCHS = 3
+NUM_WORKERS = 2
+IMAGE_HEIGHT = 160
+IMAGE_WIDTH = 240
+PIN_MEMORY = True
+LOAD_MODEL = False
+TRAIN_IMG_DIR = r"C:\Users\damti\OneDrive - The University of Technology\Desktop\Study\Do an tot nghiep\Working space _ DATN\contruction\data\train"
+TRAIN_MASK_DIR = r"C:\Users\damti\OneDrive - The University of Technology\Desktop\Study\Do an tot nghiep\Working space _ DATN\contruction\data\train_masks"
+VAL_IMG_DIR = ""
+VAL_MASK_DIR = ""
+
+
+def train_fn(loader, model, optimizer, loss_fn, scaler):
+    # use tqdm for progress bar
+    loop = tqdm(loader)
+
+    for batch_idx, (data, targets) in enumerate(loop):
+        data = data.to(device=DEVICE)
+        targets = targets.float().unsqueeze(1).to(device=DEVICE)
+
+        # forward
+        with torch.cuda.amp.autocast():
+            predictions = model(data)
+            loss = loss_fn(predictions, targets)
+
+        # backward
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        # update tqdm loop
+        loop.set_postfix(loss=loss.item())
+
+
+def main():
+
+    train_transform = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width = IMAGE_WIDTH),
+            A.Normalize(
+                mean=[0.,0.,0.],
+                std=[1.,1.,1.],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ]
+    )
+
+    val_transform = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Normalize(
+                mean=[0., 0., 0.],
+                std=[1., 1., 1.],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ]
+    )
+
+
+
+    model = UNET(in_channels=3, out_channels=1).to(DEVICE)
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    train_loader, val_loader = get_loaders(
+        train_dir=TRAIN_IMG_DIR,
+        train_maskdir=TRAIN_MASK_DIR,
+        batch_size=BATCH_SIZE,
+        train_transform=train_transform,
+        val_transform=val_transform,
+        num_workers=NUM_WORKERS,
+        pin_memory=PIN_MEMORY,
+    )
+
+    if LOAD_MODEL:
+        load_checkpoint(torch.load("my_checkpoint.pth.tar"), model)
+
+    scaler = torch.cuda.amp.GradScaler()
+
+    for epoch in range(NUM_EPOCHS):
+        train_fn(train_loader, model, optimizer, loss_fn, scaler)
+
+        # save model
+        checkpoint = {
+            "state_dict": model.state_dict(),
+            "optimizer": optimizer.state_dict(),
+        }
+
+        now = datetime.now()
+        # save_checkpoint(checkpoint, filename=f'my_checkpoint_{now.strftime("%Y_%m_%d_%H_%M_%S")}')
+        save_checkpoint(checkpoint, filename=f'my_checkpoint.pth.tar')
+
+        # check acc
+        check_accuracy(val_loader, model, device=DEVICE)
+
+        # print example
+        save_predictions_as_imgs(val_loader, model, folder="saved_images/", device=DEVICE)
+
+if __name__ == "__main__":
+    main()
